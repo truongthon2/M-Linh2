1cd205a7534077647fc7b55e842977142eccf9e4
truongthon2 <114482297+truongthon2@users.noreply.github.com>
2022-09-27 03:16:10 -0700

    s

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Alice",
-    introTitle: 'Alice',
+    introTitle: 'Alice <3',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
